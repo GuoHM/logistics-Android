fd94ec089a6bd3d9e3edb3238746ae5dae43ad90
shuffle <282650205@qq.com>
2017-10-12 17:00:57 +0800

    modify JsonHelper

--- server/src/util/JsonHelper.java
+++ server/src/util/JsonHelper.java
@@ -1,30 +1,31 @@
 package util;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 import javax.servlet.http.HttpServletResponse;
 
 public class JsonHelper {
 	public Map<String, Object> data;
-	
-	public void init(HttpServletResponse response){
-		response.setContentType("text/json;charset=utf-8");
-		response.setCharacterEncoding("UTF-8");
-	}
+	private HttpServletResponse response;
 
+	public JsonHelper(HttpServletResponse response){
+		this.response=response;
+		this.response.setContentType("text/json;charset=utf-8");
+		this.response.setCharacterEncoding("UTF-8");
+	}
+	
 	public void put(String name,Object object){
 		data = new HashMap<String , Object>();
 		data.put(name, object);
 	}
 	
-	public void output(HttpServletResponse response) throws IOException{
+	public void output() throws IOException{
 		byte[] jsonBytes = this.data.toString().getBytes("utf-8");
-		response.setContentLength(jsonBytes.length);
-		response.getOutputStream().write(jsonBytes);
-		response.getOutputStream().flush();
-		response.getOutputStream().close();
+		this.response.setContentLength(jsonBytes.length);
+		this.response.getOutputStream().write(jsonBytes);
+		this.response.getOutputStream().flush();
+		this.response.getOutputStream().close();
 	}
 
 }
