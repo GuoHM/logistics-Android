93314cf1f89bdcc8ccff3b78b1c38397f40aed11
xiaoxiao1995 <2507970341@qq.com>
2017-10-12 15:58:45 +0800

    first upload client code

--- client/gen/com/example/logistics_android/R.java
+++ client/gen/com/example/logistics_android/R.java
@@ -24,7 +24,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int RelativeLayout1=0x7f08000a;
+        public static final int action_settings=0x7f08000f;
+        public static final int admin=0x7f080009;
+        public static final int cancel=0x7f080008;
+        public static final int confirm=0x7f08000d;
+        public static final int districtCenter=0x7f080007;
+        public static final int goodsNo=0x7f08000e;
+        public static final int goodsNumberField=0x7f08000c;
+        public static final int layout02=0x7f080000;
+        public static final int ok=0x7f080005;
+        public static final int passwordField=0x7f080004;
+        public static final int passwordLable=0x7f080003;
+        public static final int provinceCenter=0x7f080006;
+        public static final int textView1=0x7f08000b;
+        public static final int userField=0x7f080001;
+        public static final int userLable=0x7f080002;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
