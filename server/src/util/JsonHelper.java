bf0afbb14521c2dcde5661db65dec045b9a3441b
shuffle <282650205@qq.com>
2017-10-17 13:33:29 +0800

    add search goods function

--- server/src/util/JsonHelper.java
+++ server/src/util/JsonHelper.java
@@ -1,31 +1,26 @@
 package util;
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import javax.servlet.http.HttpServletResponse;
-
 public class JsonHelper {
-	public Map<String, Object> data;
-	private HttpServletResponse response;
-
-	public JsonHelper(HttpServletResponse response){
-		this.response=response;
-		this.response.setContentType("text/json;charset=utf-8");
-		this.response.setCharacterEncoding("UTF-8");
-	}
-	
-	public void put(String name,Object object){
-		this.data = new HashMap<String , Object>();
-		this.data.put(name, object);
-	}
-	
-	public void output() throws IOException{
-		byte[] jsonBytes = this.data.toString().getBytes("utf-8");
-		this.response.setContentLength(jsonBytes.length);
-		this.response.getOutputStream().write(jsonBytes);
-		this.response.getOutputStream().flush();
-		this.response.getOutputStream().close();
-	}
-
+    public Map<String, Object> data=new HashMap<String,Object>();
+    private HttpServletResponse response;
+    public JsonHelper(HttpServletResponse response){
+        this.response=response;
+        this.response.setContentType("text/json;charset=utf-8");
+        this.response.setCharacterEncoding("UTF-8");
+    }
+    
+    public void put(String name,Object object){
+        this.data.put(name, object);
+    }
+    
+    public void output() throws IOException{
+        byte[] jsonBytes = this.data.toString().getBytes("utf-8");
+        this.response.setContentLength(jsonBytes.length);
+        this.response.getOutputStream().write(jsonBytes);
+        this.response.getOutputStream().flush();
+        this.response.getOutputStream().close();
+    }
 }
